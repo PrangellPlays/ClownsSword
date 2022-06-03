233184d73ff1e360474d1458ef8aed0370ed8553
PrangellPlays <ewanprangell@gmail.com>
2022-06-03 13:10:26 +0100

    Stuff

--- src/main/java/net/prangellplays/clownsword/ClownSword.java
+++ src/main/java/net/prangellplays/clownsword/ClownSword.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ClownSword implements ModInitializer {
-	public static final String MOD_ID = "clownsword";
+	public static final String MOD_ID = "PrangellsContent";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
